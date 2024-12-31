87c758adb68eddf721c1a98e984cba0bafd391cd
emre_coruhlu <129585785+emrecoruhlu0@users.noreply.github.com>
2024-12-31 14:24:52 +0300

    Delete deneme.py

--- deneme.py
+++
@@ -1,9 +0,0 @@
-orijinal = {'isim': 'Ali', 'yas': 25, 'adres': {'sehir': 'İstanbul', 'ilce': 'Kadıköy'}}
-kopya = orijinal.copy()
-
-# Orijinal sözlükte değişiklik
-orijinal['isim'] = 'Veli'
-orijinal['adres']['sehir'] = 'Ankara'
-
-print("Orijinal:", orijinal)
-print("Kopya:", kopya)  # 'isim' değişmez ama 'adres' aynı referansa sahip olduğu için değişir
